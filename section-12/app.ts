e2eab57031e26f82993f8ff5daadad869a95cf83
chongzixin <chongzixin@zis-macbook-pro.lan>
2021-11-09 09:26:12 +0800

    finished section 12

--- section-12/app.ts
+++ section-12/app.ts
@@ -1,2 +1,17 @@
-import _ from 'lodash';
-console.log(_.shuffle([1,2,3]));
+/* 
+    USING pre-existing libraries
+*/
+
+// import _ from 'lodash';
+// console.log(_.shuffle([1,2,3]));
+
+/*
+    USING CLASS-TRANSFORMER
+*/
+import { Product } from './product.model';
+
+const p1 = new Product('A book', 12.99);
+console.log(p1.getInformation());
+
+// class-transformer is a library that makes an array of fields into a class
+
--- section-12/webpack.config.js
+++ section-12/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   devtool: "inline-source-map",
   devServer: {
     historyApiFallback: true,
+    hot: true,
   },
   module: {
     rules: [
